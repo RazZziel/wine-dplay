ffdaa58f557915de78f9459cd39c36355f375df4
Ismael Barros <razielmine@gmail.com>
2009-07-20 14:51:50 +0200

    dplayx: Fixed typo

--- dlls/dplayx/dplay.c
+++ dlls/dplayx/dplay.c
@@ -2799,7 +2799,7 @@ static HRESULT DP_IF_GetPlayerName
     lpName->u1.lpszShortNameA = NULL;
   }
 
-  if( lpPList->lpPData->name.u1.lpszShortNameA )
+  if( lpPList->lpPData->name.u2.lpszLongNameA )
   {
     strcpy( ((char*)lpName)+lpPList->lpPData->name.dwSize,
             lpPList->lpPData->name.u2.lpszLongNameA );
